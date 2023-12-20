401cb05f79b1fe0f90932c8c7ac99b8bca66a781
Rickytime60 <144464210+Rickytime60@users.noreply.github.com>
2023-12-20 14:29:29 -0600

    maybe added coreprotect support

--- src/com/aurgiyalgo/SlimefunNukes/SFNukesUtils.java
+++ src/com/aurgiyalgo/SlimefunNukes/SFNukesUtils.java
@@ -6,6 +6,31 @@ import java.util.Random;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+
+private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 9) {
+            return null;
+        }
+
+        return CoreProtect;
+}
+
 
 public class SFNukesUtils {
 	
